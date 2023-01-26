70a8959bbc2aed013516fb0dcfafd21511b06742
Hasmelash95 <hermonasmelash@yahoo.com>
2023-01-26 11:36:34 +0000

    Fun times

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+def hello_babe(w, x, y, z):
+    return w + x + y + z
+
+print(hello_babe('strawbery ', 'milkshake ', 'is ', 'okay'))
+
+
+def car_name(variable, value):
+    variable = value
+    return variable
+
+print(car_name('carname', 'Volvo'))
+print(car_name('carname', 'VW'))
